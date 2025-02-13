b8cd922bd612b9d9cb8d703a94c4f2962bf66e16
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-02-13 14:33:38 +0530

    My LinkedList store it

---
+++ src/Implementation/LinkedListclient.java
@@ -0,0 +1,26 @@
+package Implementation;
+
+public class LinkedListclient {
+    public static void main(String[] args) throws Exception {
+        Linkedlist list = new Linkedlist();
+        list.addfirst(10);
+        list.addfirst(20);
+        list.addfirst(30);
+
+        list.display();
+        list.addlast(40);
+        list.display();
+        list.addatindex(88 , 2);
+        list.display();
+        System.out.println(list.Getatindex(2));
+        System.out.println(list.getfirst());
+        System.out.println(list.getlast());
+        list.removefirst();
+        list.display();
+        list.removelast();
+        list.display();
+        list.removeatindex(1);
+        list.display();
+        System.out.println();
+    }
+}
