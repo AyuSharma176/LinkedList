51726df508e87a0fc00fc8b351a0526d16e23a04
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-02-15 03:12:40 +0530

    My LinkedList store it

---
+++ src/Basic_QUES/Reverse_of_LL.java
@@ -0,0 +1,27 @@
+package Basic_QUES;
+
+import Implementation.Linkedlist;
+
+public class Reverse_of_LL {
+
+//
+//        public static ListNode reverseList(ListNode head) {
+//            if (head == null) {
+//                return head;
+//            }
+//            ListNode prev = head;
+//            ListNode curr = prev.next;
+//
+//            while (curr != null) {
+//                ListNode ahead = curr.next;
+//                curr.next = prev;
+//                prev = curr;
+//                curr = ahead;
+//            }
+//            head.next = null;
+//            head = prev;
+//
+//            return head;
+//        }
+
+}
