3e4eb54a478f47ae7403794b83129f80d823db6c
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-05 14:01:46 +0530

    My LinkedList store it

---
+++ src/Basic_QUES/LL_cycle.java
@@ -0,0 +1,20 @@
+package Basic_QUES;
+
+import java.util.Scanner;
+
+public class LL_cycle {
+    public static void main(String[] args) {
+
+//        ListNode slow=head;
+//        ListNode fast = head;
+//
+//        while(fast!=null && fast.next!=null){
+//            slow=slow.next;
+//            fast=fast.next.next;
+//            if(slow==fast){
+//                return true;
+//            }
+//        }
+//        return false;
+    }
+}
