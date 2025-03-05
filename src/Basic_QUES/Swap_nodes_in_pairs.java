834a7d2a01790f977d12177ed61ae1aade0915ca
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-05 09:45:08 +0530

    My LinkedList store it

--- src/Basic_QUES/Swap_nodes_in_pairs.java
+++ src/Basic_QUES/Swap_nodes_in_pairs.java
@@ -12,4 +12,28 @@ public class Swap_nodes_in_pairs {
 //            return news;
 //        }
 //    }
+    //using while loop
+//    if(head==null || head.next==null){
+//        return head;
+//    }
+//    ListNode h1=head;
+//    ListNode h2=head.next;
+//
+//    while(h1.next!=null && h2.next!=null){
+//        int temp=h1.val;
+//        h1.val=h2.val;
+//        h2.val=temp;
+//        h1=h2.next;
+//        if(h1.next!=null){
+//            h2=h1.next;
+//        }else{
+//            return head;
+//        }
+//
+//    }
+//    int temp=h1.val;
+//    h1.val=h2.val;
+//    h2.val=temp;
+//
+//     return head;
 }
