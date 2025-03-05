e65bf0e78011f57c293adbf15fd18d77c70b7489
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-05 18:22:23 +0530

    My LinkedList store it

---
+++ src/Basic_QUES/remove_duplicates.java
@@ -0,0 +1,18 @@
+package Basic_QUES;
+
+public class remove_duplicates {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        ListNode h1=head;
+//
+//        while(h1!=null && h1.next!=null ){
+//            if(h1.val==h1.next.val){
+//                h1.next=h1.next.next;
+//
+//            }
+//            else{
+//                h1=h1.next;
+//            }
+//        }
+//        return head;
+//    }
+}
