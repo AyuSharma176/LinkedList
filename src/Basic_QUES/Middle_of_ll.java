39adbc0f81f25ecad5e647b6ceb24effaa844047
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-14 16:47:13 +0530

    My LinkedList store it

---
+++ src/Basic_QUES/Middle_of_ll.java
@@ -0,0 +1,13 @@
+package Basic_QUES;
+
+public class Middle_of_ll {
+//    public static Middleofll(ListNode head){
+//        ListNode slow = head;
+//        ListNode fast= head;
+//        while(fast!=null && fast.next!=null){
+//              slow=slow.next;
+//              fast=fast.next.next;
+//        }
+//        return slow;
+//    }
+}
