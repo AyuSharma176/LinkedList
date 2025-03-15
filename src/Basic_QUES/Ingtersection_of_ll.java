59bc4989874f1bff9bca75eeeacea5fdb096d753
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-15 17:40:15 +0530

    My LinkedList store it

--- src/Basic_QUES/Ingtersection_of_ll.java
+++ src/Basic_QUES/Ingtersection_of_ll.java
@@ -18,5 +18,46 @@ public class Ingtersection_of_ll {
 //            }
 //        }
 //        return A;
+
+
+
+
+        // Another solution
+//        if (headA == null || headB == null) return null;
+//        int size1=0;
+//        int size2=0;
+//        ListNode check1=headA;
+//        ListNode check2=headB;
+//        while(check1!=null){
+//            check1=check1.next;
+//            size1++;
+//        }
+//        while(check2!=null){
+//            check2=check2.next;
+//            size2++;
+//        }
+//        int diff=Math.abs(size1-size2);
+//        ListNode first=headA;
+//        if(size1>size2){
+//            while(diff-->0){
+//                first=first.next;
+//            }
+//
+//        }
+//        ListNode second=headB;
+//        if(size2>size1){
+//            while(diff-->0){
+//                second=second.next;
+//            }
+//
+//        }
+//        while(first!=null && second!=null){
+//            if(first==second){
+//                return first;
+//            }
+//            first=first.next;
+//            second=second.next;
+//        }
+//        return null;
     }
 }
