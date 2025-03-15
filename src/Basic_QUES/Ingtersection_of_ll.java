6a5fba2631e25981c2eb14ba1f1bc1b2c115ae8e
Ayush Sharma <ayushoffinfo17@gmail.com>
2025-03-15 17:39:15 +0530

    My LinkedList store it

---
+++ src/Basic_QUES/Ingtersection_of_ll.java
@@ -0,0 +1,22 @@
+package Basic_QUES;
+
+public class Ingtersection_of_ll {
+    public static void main(String[] args) {
+//        ListNode A=headA;
+//        ListNode B=headB;
+//        while(A!=B){
+//            if(A==null){
+//                A=headB;
+//            }else{
+//                A=A.next;
+//            }
+//            if(B==null){
+//                B=headA;
+//            }
+//            else{
+//                B=B.next;
+//            }
+//        }
+//        return A;
+    }
+}
